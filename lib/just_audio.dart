a4afe5e48e26e480e166dbf1409588e47be3ba5a
stonegate <xijieyin@gmail.com>
2020-07-19 23:59:17 +0800

    Improve cache manage

--- lib/just_audio.dart
+++ lib/just_audio.dart
@@ -190,9 +190,8 @@ class AudioPlayer {
   /// Loads audio media from a URL and completes with the duration of that
   /// audio, or null if this call was interrupted by another call so [setUrl],
   /// [setFilePath] or [setAsset].
-  Future<Duration> setUrl(final String url, final int cacheMax) async {
-    final String cacheDir = (await getApplicationDocumentsDirectory()).path;
-    _durationFuture = _invokeMethod('setUrl', [url, cacheDir, cacheMax])
+  Future<Duration> setUrl(final String url, {final int cacheMax = 0}) async {
+    _durationFuture = _invokeMethod('setUrl', [url, cacheMax])
         .then((ms) => ms == null ? null : Duration(milliseconds: ms));
     _duration = await _durationFuture;
     _durationSubject.add(_duration);
